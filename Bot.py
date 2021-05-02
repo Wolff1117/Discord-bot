858cb7d4ccba85f87e7c8f73637644508373a779
Wolff1117 <1117kelton@gmail.com>
2021-05-01 21:17:34 -0700

    commin too

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord, json, os
+from random import randint
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -7,9 +8,6 @@ TOKEN = os.getenv("TOKEN")
 
 client = commands.Bot(command_prefix = "dip!")
 client.remove_command('help')
-with open('QA.json','r') as file:
-    qa = json.loads(file.read())
-
 
 @client.event
 async def on_message(message):
@@ -27,6 +25,7 @@ async def autoResponse(ctx):
 async def on_ready():
     print(f"{client.user.name} connected to Discord!\n")
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Hentai'))
+    await reload()
 
 @client.command(name = "test")
 async def test(ctx):
@@ -35,21 +34,53 @@ async def test(ctx):
 @client.group(invoke_without_command=True)
 async def help(ctx):
     embed = discord.Embed(title='help',description='dip!help <command> for knowledge',color=0xf40bd5)
-    embed.add_field(name='user commands',value='help')
+    embed.add_field(name='user commands',value='help \nfunny')
     embed.add_field(name='admin commands',value='reload')
     await ctx.send(embed=embed)
 
 @help.command(name='reload')
-async def help_reload(ctx): await ctx.send(embed=discord.Embed(title='reload',description='placeholder_text',color=0xf40bd5).add_field(name='Syntax',value='dip!reload'))
+async def help_reload(ctx): await ctx.send(embed=discord.Embed(title='reload',description='Reloads auto-responses',color=0xf40bd5).add_field(name='Syntax',value='dip!reload'))
 
 @help.command(name='help')
-async def help_reload(ctx): await ctx.send(embed=discord.Embed(title='help',description='I think you already found out how to use this.',color=0xf40bd5).add_field(name='Syntax',value='dip!help'))
+async def help_help(ctx): await ctx.send(embed=discord.Embed(title='help',description='I think you already found out how to use this.',color=0xf40bd5).add_field(name='Syntax',value='dip!help'))
+
+@help.command(name='funny')
+async def help_funny(ctx): await ctx.send(embed=discord.Embed(title='funny',description='Sends a random image from the haha funny image folder.',color=0xf40bd5).add_field(name='Syntax',value='dip!funny'))
 
 @client.command(name = "reload")
-async def reload(ctx):
-    global qa
+async def reload():
+    global qa, hahaFunnyImages,hahaFunnyVideos, hahaFunnySounds
     with open('QA.json','r') as file:
         qa = json.loads(file.read())
-    await ctx.send("done")
+    hahaFunnyImages = []
+    for root, dirs, files in os.walk(fr"D:\haha funny image"):
+        for filename in files:
+            hahaFunnyImages.append(f'{root}\\{filename}')
+
+    hahaFunnyVideos = []
+    for root, dirs, files in os.walk(fr"D:\haha funny video"):
+        for filename in files:
+            hahaFunnyVideos.append(f'{root}\\{filename}')
     
+    hahaFunnySounds = []
+    for root, dirs, files in os.walk(fr"D:\haha funny sound"):
+        for filename in files:
+            hahaFunnySounds.append(f'{root}\\{filename}')
+
+@client.group(invoke_without_command=True)
+async def funny(ctx):
+    await ctx.send(embed=discord.Embed(title='funny',description='dip!funny image\ndip!funny video\ndip!funny sound',color=0xf40bd5))
+
+@funny.command(name="image")
+async def funnyImage(ctx):
+    await ctx.send(file=discord.File(hahaFunnyImages[randint(0,len(hahaFunnyImages))]))
+
+@funny.command(name="video")
+async def funnyVideo(ctx):
+    await ctx.send(file=discord.File(hahaFunnyVideos[randint(0,len(hahaFunnyVideos))]))
+
+@funny.command(name="sound")
+async def funnySound(ctx):
+    await ctx.send(file=discord.File(hahaFunnySounds[randint(0,len(hahaFunnySounds))]))
+
 client.run(TOKEN)
--- dwbi.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-STEAM_APIKEY = os.getenv("STEAM_APIKEY")
-DISCORD_TOKEN = os.getenv("TOKEN")
