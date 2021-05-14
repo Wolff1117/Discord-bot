9fbf5aa6c8cdf39669600b2f61e0a6112214cc73
Wolff1117 <1117kelton@gmail.com>
2021-05-13 20:16:49 -0700

    it is done

--- Bot.py
+++ Bot.py
@@ -21,8 +21,6 @@ class event(commands.Cog):
     async def on_message(self,message):
         if message.author == client.user: return
         if message.content in qa and not message.author.bot: await message.channel.send(qa[message.content])
-        try: await client. process_commands(message)
-        except Exception: await message.channel.send('invalid command')
     @commands.Cog.listener()
     async def on_command_error(self,ctx,error): await ctx.send('no')
 class command(commands.Cog):
@@ -33,9 +31,9 @@ class command(commands.Cog):
         globals()['qa'] = json.loads(open('QA.json','r').read())
         globals()['entries'] = json.loads(open('Entries.json','r').read())["Entries"]
         globals()['hahaFunnyImages'] = []; globals()['hahaFunnyVideos'] = []; globals()['hahaFunnySounds'] = []
-        for root,files in os.walk(fr"D:\haha funny image"): [hahaFunnyImages.append(f'{root}\\{filename}') for filename in files]  
-        for root,files in os.walk(fr"D:\haha funny video"): [hahaFunnyVideos.append(f'{root}\\{filename}') for filename in files]
-        for root,files in os.walk(fr"D:\haha funny sound"): [hahaFunnySounds.append(f'{root}\\{filename}') for filename in files]
+        for root,dirs,files in os.walk(fr"D:\haha funny image"): [hahaFunnyImages.append(f'{root}\\{filename}') for filename in files]  
+        for root,dirs,files in os.walk(fr"D:\haha funny video"): [hahaFunnyVideos.append(f'{root}\\{filename}') for filename in files]
+        for root,dirs,files in os.walk(fr"D:\haha funny sound"): [hahaFunnySounds.append(f'{root}\\{filename}') for filename in files]
     @commands.command(name="entry")
     async def entry(self,ctx,index=None):
         if ctx.author.id == 364237299275399168: await ctx.send(entries[2]); return
@@ -43,12 +41,15 @@ class command(commands.Cog):
         except: await ctx.send(f'Entry must be between 1 and {len(entries)-1}'); return
         if index>len(entries): await ctx.send("Entry Out of Range"); return
         await ctx.send(entries[index])
+    @commands.command(name="comfort")
+    async def dm(self,ctx):
+        await ctx.author.send('Hey, how was your day')
 class help(commands.Cog):
     def __init__(self,client): self.client = client
     @commands.group(invoke_without_command=True)
     async def help(self,ctx):
         embed = discord.Embed(title='help',description='dip!help <command> for knowledge',color=0xf40bd5)
-        embed.add_field(name='user commands',value='help \nfunny \nentry')
+        embed.add_field(name='user commands',value='help \nfunny \nentry \ncomfort')
         embed.add_field(name='admin commands',value='reload')
         await ctx.send(embed=embed)
     @help.command(name='reload')
@@ -59,15 +60,17 @@ class help(commands.Cog):
     async def help_funny(self,ctx): await ctx.send(embed=discord.Embed(title='funny',description='Sends a random image from the haha funny image folder.',color=0xf40bd5).add_field(name='Syntax',value='dip!funny'))
     @help.command(name='entry')
     async def help_entry(self,ctx): await ctx.send(embed=discord.Embed(title='entry',description='Displays entry of number given from the Manifest',color=0xf40bd5).add_field(name='Syntax',value=f'dip!entry (1-{len(entries)-1})'))
+    @help.command(name='comfort')
+    async def help_comfort(self,ctx): await ctx.send(embed=discord.Embed(title='comfort',description='Sends something nice',color=0xf40bd5).add_field(name='Syntax',value='dip!comfort'))
 class funny(commands.Cog):
     def __init__(self,client): self.client = client
     @commands.group(invoke_without_command=True)
     async def funny(self,ctx): await ctx.send(embed=discord.Embed(title='funny',description='dip!funny image\ndip!funny video\ndip!funny sound',color=0xf40bd5))
-    @commands.command(name="image")
+    @funny.command(name="image")
     async def funnyImage(self,ctx): await ctx.send(file=discord.File(hahaFunnyImages[randint(0,len(hahaFunnyImages))]))
-    @commands.command(name="video")
+    @funny.command(name="video")
     async def funnyVideo(self,ctx): await ctx.send(file=discord.File(hahaFunnyVideos[randint(0,len(hahaFunnyVideos))]))
-    @commands.command(name="sound")
+    @funny.command(name="sound")
     async def funnySound(self,ctx): await ctx.send(file=discord.File(hahaFunnySounds[randint(0,len(hahaFunnySounds))]))
 
 client.add_cog(command(client))
